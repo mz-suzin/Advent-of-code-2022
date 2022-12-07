f39978671d716ad9e536d0a942fa56756df23f0b
mz-suzin <mzsuzin@gmail.com>
2022-12-07 10:39:51 -0300

    final commit for both challenges. Nice

--- Day4/solution.js
+++ Day4/solution.js
@@ -1,6 +1,41 @@
 //coded by mz-suzin
 const fs = require('fs');
 
-const data = fs.readFileSync('input_short.txt', 'utf8').toString().split('\r\n');
+const data = fs.readFileSync('input.txt', 'utf8').toString().split('\r\n');
 
-console.log(data);
+const solution = (data) => {
+    let fullyOverlaping = 0;
+    let partialOverlaping = 0;
+
+    const getSections = (elf) => {
+        const [firstSection, lastSection] = elf.split('-');
+        return [+firstSection, +lastSection];
+    }
+    
+    data.map((pair) => {
+        [firstElf, secondElf] = pair.split(',');
+
+        [firstSection, lastSection] = getSections(firstElf);
+        [firstSection2, lastSection2] = getSections(secondElf);
+
+        // console.log(firstSection, lastSection, firstSection2, lastSection2);
+
+        //fully overlaping sections:
+        if ((firstSection >= firstSection2 && lastSection <= lastSection2) || (firstSection <= firstSection2 && lastSection >= lastSection2)){
+            fullyOverlaping++;
+        }
+
+        //partial overlaping sections:
+        if (firstSection <= lastSection2 && lastSection >= firstSection2){
+            // console.log(firstSection, lastSection, firstSection2, lastSection2);
+            partialOverlaping++;
+        }
+
+    })
+    console.log(`the total number of fully overlaping sections is: ${fullyOverlaping}`);
+    console.log(`the number of partial overlaping sections is: ${partialOverlaping}`);
+}
+
+solution(data);
+
+// console.log(indexOf)
