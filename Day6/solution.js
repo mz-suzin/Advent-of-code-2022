ccd695d96454fbf975c111a70a341279ea88c07c
mz-suzin <mzsuzin@gmail.com>
2022-12-08 16:16:13 -0300

    adding initial docs for day 6 challenge- re

---
+++ Day6/solution.js
@@ -0,0 +1,10 @@
+//coded by mz-suzin
+const fs = require('fs');
+
+const solution = (data) => {
+    
+}
+
+const data = fs.readFileSync('input.txt', 'utf8').toString().split('\r\n');
+
+solution(data);
