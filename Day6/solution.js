1a080ab577c6ba14e63957eed36f0b25d6eb112c
mz-suzin <mzsuzin@gmail.com>
2022-12-09 16:45:28 -0300

    Day 6 completed, huge success and Im proud of my work :)

--- Day6/solution.js
+++ Day6/solution.js
@@ -1,10 +1,59 @@
 //coded by mz-suzin
 const fs = require('fs');
 
-const solution = (data) => {
-    
+const compareCharacters = (string, iterator) => {
+    areDifferent = true;
+    for (let k = iterator; k >= 1; k--){
+        for (let j = k-1; j >= 0; j--){
+            if (string[k] === string[j]) {
+                areDifferent = false;
+                return areDifferent;
+            }
+        }
+    }
+    return areDifferent;
 }
 
-const data = fs.readFileSync('input.txt', 'utf8').toString().split('\r\n');
+const solution = (data) => {
+    let startOfPackage = 0;
+    let startOfMessage = 0;
+    let stringTest = '';
+    let index = 0;
+    
+    //PART1
+    //iterate between all elements of input
+    for (index = 0; index < data.length - 3; index++) {
+        
+        //separate the test string into slices of 4, starting from index
+        stringTest = data.slice(index, index + 4).toString();
+
+        
+        //compare if any of those 4 characters are repeated doing the less amount of iterations possible
+        if(compareCharacters(stringTest, 3)){
+            startOfPackage = index + 4;
+            break;
+        }
+    }
+
+    //PART2
+    //iterate between all elements of input -- It should start from Start of Package huh? 
+    for (index = 0; index < data.length - 3; index++) {
+
+        //separate the test string into slices of 14, starting from index
+        stringTest = data.slice(index, index + 14).toString();
+    
+        //compare if any of those 14 characters are repeated doing the less amount of iterations possible
+        if(compareCharacters(stringTest, 13)){
+            startOfMessage = index + 14;
+            break;
+        }
+    }
+
+    console.log(`The message should start at character number __${startOfPackage}__`);
+    console.log(`The message started at character number __${startOfMessage}__`);
+}
+        
+
+const data = fs.readFileSync('input.txt', 'utf8').toString();
 
 solution(data);
