a047d445ee2928253e25a61f06569956d3b6bdb0
mz-suzin <mzsuzin@gmail.com>
2022-12-13 15:36:46 -0300

    inital commit for day 8 challenge

---
+++ Day8/solution.js
@@ -0,0 +1,10 @@
+//coded by mz-suzin
+const fs = require('fs');
+
+const calculate = (data) => {
+
+}
+
+const data = fs.readFileSync('input.txt', 'utf-8').toString().trim().split('\r\n');
+
+calculate(data);
